f5f4a2af90bfcfb5cfd22f4283928c3c2c9369f0
Batzaya <batzaya@Batzaya.local>
2023-11-21 21:14:23 +0800

    personFormView update

--- src/main/java/com/example/application/views/PersonFormView.java
+++ src/main/java/com/example/application/views/PersonFormView.java
@@ -9,10 +9,11 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
@@ -35,12 +36,11 @@ public class PersonFormView extends FormLayout {
     TextField lastName = new TextField("Last name");
     TextField userName = new TextField("User name");
     EmailField email = new EmailField("Email");
+    String clickBtnName;
 
     Button save = new Button("Save");
-    Button delete = new Button("Delete");
+    Button edit = new Button("Edit");
     Button close = new Button("Cancel");
-    // Other fields omitted
-    Binder<User> binder = new Binder<>(User.class);
 
     public PersonFormView(UserService userService,
                           AuthenticatedUser authenticatedUser,
@@ -48,28 +48,14 @@ public class PersonFormView extends FormLayout {
         this.authenticatedUser = authenticatedUser;
         this.userRepository = userRepository;
         this.userService = userService;
-        addClassName("person-form");
-        add(
-                firstName,
-                lastName,
-                userName,
-                email,
-                createButtonsLayout());
-
+        this.clickBtnName = close.getText().toString();
+        clickBtnListener();
+        removeAll();
+        textFiedSetReadOnly(true);
+        formView();
         initBinder();
     }
 
-    private Component createButtonsLayout() {
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
-        close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-
-        save.addClickShortcut(Key.ENTER);
-        close.addClickShortcut(Key.ESCAPE);
-
-        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
-        return new HorizontalLayout(save, delete, close);
-    }
     private void initBinder(){
         Optional<UserDetails> u = authenticatedUser.getAutenticatedUser();
         System.out.println("Person username: " + u.get().getUsername());
@@ -80,10 +66,67 @@ public class PersonFormView extends FormLayout {
         this.userName.setValue(userInfo.getUserName());
         this.email.setValue(userInfo.getEmail());
     }
-    public void setBean(User user){
-        binder.setBean(user);
+    private void clickBtnListener(){
+        close.addClickListener(e -> {
+           clickBtnName = close.getText().toString();
+           removeAll();
+           textFiedSetReadOnly(true);
+           formView();
+        });
+        edit.addClickListener(e -> {
+            clickBtnName = edit.getText().toString();
+            removeAll();
+            textFiedSetReadOnly(false);
+            formView();
+        });
     }
-    public Optional<User> getBean(){
-        return Optional.ofNullable(binder.getBean());
+    private void textFiedSetReadOnly(boolean val){
+        firstName.setReadOnly(val);
+        lastName.setReadOnly(val);
+        userName.setReadOnly(val);
+        email.setReadOnly(val);
+    }
+    private Component createButtonsLayout(String btnName) {
+
+        if(edit.getText().toString().equals(btnName)){
+            save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+            close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+            save.addClickShortcut(Key.ENTER);
+            close.addClickShortcut(Key.ESCAPE);
+            return new HorizontalLayout(save, close);
+        }else if(close.getText().toString().equals(btnName)){
+            edit.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+            return new HorizontalLayout(edit);
+        }else {
+           return new HorizontalLayout();
+        }
+    }
+    private void formView(){
+        addClassName("person-form");
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(
+                this.firstName,
+                this.lastName,
+                this.userName,
+                this.email
+        );
+        formLayout.setResponsiveSteps(
+                new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep("600px", 2)
+        );
+
+        formLayout.getStyle().set("max-width", "75%");
+        formLayout.getStyle().set("width", "100%");
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout(formLayout);
+        horizontalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        VerticalLayout verticalLayout = new VerticalLayout(horizontalLayout, createButtonsLayout(clickBtnName));
+        verticalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        verticalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        add(verticalLayout);
     }
 }
+
