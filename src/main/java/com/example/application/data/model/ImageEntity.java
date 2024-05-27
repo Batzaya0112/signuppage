9a3da9a03f2db9cc32ed1ab56757f00ff5d0539d
Batzaya <batzaya@Batzayas-MacBook-Pro.local>
2024-05-27 21:17:33 +0800

    file upload code update

--- src/main/java/com/example/application/data/model/ImageEntity.java
+++ src/main/java/com/example/application/data/model/ImageEntity.java
@@ -11,17 +11,20 @@ import jakarta.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="image_entity")
+@Table(name = "image_entity")
 public class ImageEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable=false)
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(nullable = false)
     @NotEmpty
     private String fileName;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     @NotEmpty
     private String uploadDirectory;
 
@@ -31,13 +34,40 @@ public class ImageEntity {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public void setFileName(String fileName){this.fileName = fileName;}
-    public String getFileName(){return this.fileName;}
-    public void setUploadDirectory(String uploadDirectory){this.uploadDirectory = uploadDirectory;}
-    public String getUploadDirectory(){return this.uploadDirectory;}
-    public void setUploadDate(LocalDateTime uploadDate){this.uploadDate = uploadDate;}
-    public LocalDateTime getUploadDate(){return this.uploadDate;}
+
+    public Long getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setUploadDirectory(String uploadDirectory) {
+        this.uploadDirectory = uploadDirectory;
+    }
+
+    public String getUploadDirectory() {
+        return this.uploadDirectory;
+    }
+
+    public void setUploadDate(LocalDateTime uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public LocalDateTime getUploadDate() {
+        return this.uploadDate;
+    }
 }
--- src/main/java/com/example/application/views/FileUploadView.java
+++ src/main/java/com/example/application/views/FileUploadView.java
@@ -1,7 +1,10 @@
 package com.example.application.views;
 
 import com.example.application.data.model.ImageEntity;
+import com.example.application.data.model.User;
+import com.example.application.data.repository.UserRepository;
 import com.example.application.data.service.FileService;
+import com.example.application.security.AuthenticatedUser;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
@@ -13,11 +16,13 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @PermitAll
@@ -26,9 +31,19 @@ import java.util.UUID;
 public class FileUploadView extends VerticalLayout {
     @Autowired
     private final FileService fileService;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AuthenticatedUser authenticatedUser;
+
     Button saveButton = new Button("Save");
 
-    public FileUploadView(FileService fileService) {
+    public FileUploadView(FileService fileService,
+            UserRepository userRepository,
+            AuthenticatedUser authenticatedUser) {
+
+        this.authenticatedUser = authenticatedUser;
+        this.userRepository = userRepository;
         this.fileService = fileService;
 
         MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
@@ -51,31 +66,39 @@ public class FileUploadView extends VerticalLayout {
         add(layout);
     }
 
-    private void saveFile(MultiFileMemoryBuffer buffer){
-        if(!buffer.getFiles().isEmpty()){
-            try{
+    private void saveFile(MultiFileMemoryBuffer buffer) {
+        if (!buffer.getFiles().isEmpty()) {
+            try {
                 String fileName = buffer.getFiles().iterator().next();
                 Path filePath = saveFileToPath(buffer.getInputStream(fileName).readAllBytes(), fileName);
                 String filePathString = filePath.toString();
                 saveToDatabase(filePathString, fileName);
                 Notification.show("File saved successfully");
-            }catch(IOException e){
+            } catch (IOException e) {
                 Notification.show("Error saving file: " + e.getMessage());
             }
-        }else{
+        } else {
             Notification.show("Файл оруулаагүй байна.");
         }
     }
-    private Path saveFileToPath(byte[] content, String fileName) throws IOException{
-        String uploadDirectory = "/Users/batzaya/Documents/Hicheel/Java/VaadinTutorial/src/main/resources/META-INF/resources/images/";
+
+    private Path saveFileToPath(byte[] content, String fileName) throws IOException {
+        String uploadDirectory = "/Users/batzaya/Documents/Huviin/Hicheel/Java/signuppage/src/main/resources/META-INF/resources/images/";
+
         String uniqueFileName = UUID.randomUUID().toString() + "_" + fileName;
         Path targetPath = Path.of(uploadDirectory, uniqueFileName);
         Files.write(targetPath, content);
         System.out.println("target path =======> " + targetPath.toString());
         return targetPath;
     }
-    private void saveToDatabase(String filePath, String fileName){
+
+    private void saveToDatabase(String filePath, String fileName) {
+        Optional<UserDetails> u = authenticatedUser.getAutenticatedUser();
+        System.out.println("Person username: " + u.get().getUsername());
+        User userInfo = userRepository.findByUsername(u.get().getUsername());
+        System.out.println("Person userInfo: " + u.get().getUsername());
         ImageEntity imageEntity = new ImageEntity();
+        imageEntity.setUserId(userInfo.getId());
         imageEntity.setUploadDirectory(filePath);
         imageEntity.setFileName(fileName);
         imageEntity.setUploadDate(LocalDateTime.now());
